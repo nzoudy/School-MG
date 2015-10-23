6c26ed3461b1f001a6faaf107ce3378594f81377
nzouda urbain <nzoudy@hotmail.fr>
2015-10-23 10:11:05 +0200

    add attributes in class Formation, Etudiant

--- src/main/java/com/ingesup/expcal/entities/Etudiant.java
+++ src/main/java/com/ingesup/expcal/entities/Etudiant.java
@@ -1,5 +1,20 @@
 package com.ingesup.expcal.entities;
 
-public class Etudiant {
+import java.io.Serializable;
+import java.util.Date;
 
+public class Etudiant implements Serializable {
+
+	private Long idEtudiant;
+	private String nomEtudiant;
+	private String prenom;
+	private Date dateNaissance;
+	private String telephone;
+	private String email;
+	private String nomEntreprise;
+	private String numSecuriteSociale; 
+	private Date dateCreationCompte;
+	private Date dateDernierModification;
+	private Long nbModification;
+	private Formation formation;
 }
--- src/main/java/com/ingesup/expcal/entities/Formation.java
+++ src/main/java/com/ingesup/expcal/entities/Formation.java
@@ -1,5 +1,13 @@
 package com.ingesup.expcal.entities;
 
-public class Formation {
+import java.util.List;
+import java.util.Date;
 
+public class Formation {
+	private Long idFormation;
+	private String nomFormation;
+	private String descriptionFormation;
+	private Date dureeEtude;
+	private Degre degre;
+	private List<Matiere> matieres;
 }
